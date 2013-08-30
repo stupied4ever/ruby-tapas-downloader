8493be7517fbd38cf556570bc97d58b8243e1d76
Leandro Facchinetti <git@leafac.com>
2013-08-29 21:16:31 -0300

    Install dependencies

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem 'mechanize'
+
+group :test do
+  gem 'rspec'
+  gem 'pry-debugger'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/ruby-tapas-downloader'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+end
